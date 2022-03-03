5f814ea13e4da43a20da6df42b48645f8f0a06ba
VladislavPaskar <vladislav.paskar@sap.com>
2022-03-03 14:23:01 +0100

    test provisioning

--- tests/fast-integration/eventing-test/provision/provision-skr.js
+++ tests/fast-integration/eventing-test/provision/provision-skr.js
@@ -10,59 +10,114 @@ const {
 } = require('../../kcp/client');
 
 const {
-  gardener,
-  keb,
 } = require('../../skr-test');
 
 const {
   getEnvOrThrow,
   debug,
 } = require('../../utils');
+const {genRandom, switchDebug} = require('../utils');
+const {KEBClient, KEBConfig} = require('../kyma-environment-broker');
+const {GardenerClient, GardenerConfig} = require('../gardener');
 
 const instanceId = process.env.INSTANCE_ID || uuid.v4();
-const runtimeName = getEnvOrThrow('RUNTIME_NAME');
-const kymaVersion = getEnvOrThrow('KYMA_VERSION');
-const kymaOverridesVersion = process.env.KYMA_OVERRIDES_VERSION || '';
-const kcp = new KCPWrapper(KCPConfig.fromEnv());
 
-let skr;
 
 describe('Provision SKR cluster', function() {
-  this.timeout(60 * 60 * 1000 * 2); // 2h
+  switchDebug(on = true);
+  const keb = new KEBClient(KEBConfig.fromEnv());
+  const gardener = new GardenerClient(GardenerConfig.fromEnv());
+
+  const suffix = genRandom(4);
+  const appName = `app-${suffix}`;
+  const runtimeName = `kyma-${suffix}`;
+  const scenarioName = `test-${suffix}`;
+  const instanceID = uuid.v4();
+  const subAccountID = keb.subaccountID;
+
+  debug(
+      `PlanID ${getEnvOrThrow('KEB_PLAN_ID')}`,
+      `SubAccountID ${subAccountID}`,
+      `InstanceID ${instanceID}`,
+      `Scenario ${scenarioName}`,
+      `Runtime ${runtimeName}`,
+      `Application ${appName}`,
+  );
+
+  // debug(
+  //   `KEB_HOST: ${getEnvOrThrow("KEB_HOST")}`,
+  //   `KEB_CLIENT_ID: ${getEnvOrThrow("KEB_CLIENT_ID")}`,
+  //   `KEB_CLIENT_SECRET: ${getEnvOrThrow("KEB_CLIENT_SECRET")}`,
+  //   `KEB_GLOBALACCOUNT_ID: ${getEnvOrThrow("KEB_GLOBALACCOUNT_ID")}`,
+  //   `KEB_SUBACCOUNT_ID: ${getEnvOrThrow("KEB_SUBACCOUNT_ID")}`,
+  //   `KEB_USER_ID: ${getEnvOrThrow("KEB_USER_ID")}`,
+  //   `KEB_PLAN_ID: ${getEnvOrThrow("KEB_PLAN_ID")}`
+  // );
+
+  // debug(
+  //   `COMPASS_HOST: ${getEnvOrThrow("COMPASS_HOST")}`,
+  //   `COMPASS_CLIENT_ID: ${getEnvOrThrow("COMPASS_CLIENT_ID")}`,
+  //   `COMPASS_CLIENT_SECRET: ${getEnvOrThrow("COMPASS_CLIENT_SECRET")}`,
+  //   `COMPASS_TENANT: ${getEnvOrThrow("COMPASS_TENANT")}`,
+  // )
+
+  // debug(
+  //   `KCP_TECH_USER_LOGIN: ${KCP_TECH_USER_LOGIN}\n`,
+  //   `KCP_TECH_USER_PASSWORD: ${KCP_TECH_USER_PASSWORD}\n`,
+  //   `KCP_OIDC_CLIENT_ID: ${KCP_OIDC_CLIENT_ID}\n`,
+  //   `KCP_OIDC_CLIENT_SECRET: ${KCP_OIDC_CLIENT_SECRET}\n`,
+  //   `KCP_KEB_API_URL: ${KCP_KEB_API_URL}\n`,
+  //   `KCP_OIDC_ISSUER_URL: ${KCP_OIDC_ISSUER_URL}\n`
+  // )
+
+  // Credentials for KCP ODIC Login
+
+  // process.env.KCP_TECH_USER_LOGIN    =
+  // process.env.KCP_TECH_USER_PASSWORD =
+  process.env.KCP_OIDC_ISSUER_URL = 'https://kymatest.accounts400.ondemand.com';
+  // process.env.KCP_OIDC_CLIENT_ID     =
+  // process.env.KCP_OIDC_CLIENT_SECRET =
+  process.env.KCP_KEB_API_URL = 'https://kyma-env-broker.cp.dev.kyma.cloud.sap';
+  process.env.KCP_GARDENER_NAMESPACE = 'garden-kyma-dev';
+  process.env.KCP_MOTHERSHIP_API_URL = 'https://mothership-reconciler.cp.dev.kyma.cloud.sap/v1';
+  process.env.KCP_KUBECONFIG_API_URL = 'https://kubeconfig-service.cp.dev.kyma.cloud.sap';
+
+  const kcp = new KCPWrapper(KCPConfig.fromEnv());
+
+  const kymaVersion = getEnvOrThrow('KYMA_VERSION');
+
+  this.timeout(60 * 60 * 1000 * 3); // 3h
   this.slow(5000);
-  before('Provision new SKR', async function() {
-    // login to kcp, required by provisionSKR method
+
+  const provisioningTimeout = 1000 * 60 * 60; // 1h
+
+  let skr;
+
+  // SKR Provisioning
+
+  it(`Perform kcp login`, async function() {
     const version = await kcp.version([]);
-    debug('Login to KCP. Version: ', version);
+    debug(version);
+
     await kcp.login();
+    // debug(loginOutput)
+  });
 
-    // define params required by provisionSKR method
-    const provisioningTimeout = 1000 * 60 * 60; // 1h
-
-    const customParams = {'kymaVersion': kymaVersion};
-    if (kymaOverridesVersion) {
-      customParams['overridesVersion'] = kymaOverridesVersion;
-    }
-
-    debug(`Parameters:\n`,
-        `runtime ID: ${instanceId}\n`,
-        `runtime name: ${runtimeName}\n`,
-        `kyma version: ${kymaVersion}\n`,
-        `custom params: ${JSON.stringify(customParams)}\n`,
-    );
-
-    // Finally, call the provision SKR method with the provided params
-    skr = await provisionSKR(
-        keb,
+  it(`Provision SKR with ID ${instanceID}`, async function() {
+    console.log(`Provisioning SKR with version ${kymaVersion}`);
+    const customParams = {
+      'kymaVersion': kymaVersion,
+    };
+    debug(`Provision SKR with Custom Parameters ${JSON.stringify(customParams)}`);
+    skr = await provisionSKR(keb,
         kcp,
         gardener,
-        instanceId,
+        instanceID,
         runtimeName,
         null,
         null,
         customParams,
-        provisioningTimeout,
-    );
+        provisioningTimeout);
   });
 
   describe('Check provisioned SKR', function() {
